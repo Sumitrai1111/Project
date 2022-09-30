97b97b187842f9cfef61b4ddeb3dd984962474b6
Sumit Rai <110342639+Sumitrai1111@users.noreply.github.com>
2022-09-30 18:35:06 +0530

    Add files via upload

---
+++ home.js
@@ -0,0 +1,7 @@
+function loadoupon(){
+        document.getElementById('coupon').style.visibility='visible';
+}
+function closeCoupon(){
+    document.getElementById('coupon').style.visibility='hidden';
+    document.getElementById('main').style.opacity='1';
+}
